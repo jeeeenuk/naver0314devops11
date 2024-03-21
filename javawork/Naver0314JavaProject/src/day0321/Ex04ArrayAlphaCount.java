16a97a644e009c07eefd77cfe72c0d18c64019ff
JeenukChung <jeenuk.chung@gmail.com>
2024-03-21 11:13:22 +0900

    3월21일수업

---
+++ javawork/Naver0314JavaProject/src/day0321/Ex04ArrayAlphaCount.java
@@ -0,0 +1,18 @@
+package day0321;
+
+import java.util.Scanner;
+
+public class Ex04ArrayAlpahCount {
+    public static void main(String[] args) {
+        /*
+        영어문장을 입력하면 알파벳 기준으로 각각의 갯수를 출력하시오.
+        Have a Nice Day!!!!!
+
+        A:3 B:0
+         */
+        Scanner sc=new Scanner(System.in);
+        String msg;
+        int []alpha=new int[26];
+        
+    }
+}
