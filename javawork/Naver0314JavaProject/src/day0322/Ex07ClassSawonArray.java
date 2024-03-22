248c157cf845e7e3c4502e3e81d462ea00670f47
JeenukChung <jeenuk.chung@gmail.com>
2024-03-22 18:49:49 +0900

    3월22일수업

---
+++ javawork/Naver0314JavaProject/src/day0322/Ex07ClassSawonArray.java
@@ -0,0 +1,45 @@
+package day0322;
+
+import java.util.Scanner;
+//Call By Reference ...
+public class Ex07ClassSawonArray {
+    // 입력받는 메서드
+    static public void inputSawonArray(Sawon []sa){
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("총 %4d 명의 사원 정보를 입력하시오.\n",sa.length);
+
+        for (int i = 0; i < sa.length; i++) {
+            System.out.print("사원명은? : ");
+            String name=sc.nextLine();
+            System.out.print("기본금은 ? : ");
+            int gibon=Integer.parseInt(sc.nextLine());
+            System.out.print("수당은 ? : ");
+            int sudang=Integer.parseInt(sc.nextLine());
+
+            sa[i] = new Sawon(); //초기값이 null 이므로 new 로 생성을 해야 메서드 호출이 가능하다
+            sa[i].setSawon(name, gibon, sudang); //생성을 안하고 호출시 널포인트오류 발생
+            System.out.println();
+        }
+    }
+    // 출력하는 메서드
+    static public void outputSawonArray(Sawon []sa){
+        System.out.println("\t\t[사원 급여 현황]\n");
+        System.out.println("번호\t사원명\t기본급\t수당\t\t실수령액");
+        System.out.println("-".repeat(35));
+
+        for (int i = 0; i < sa.length; i++) {
+            System.out.printf("%s\t%s\t%d\t%d\t%d\n",i+1,sa[i].getName(),sa[i].getGibon(),sa[i].getSudang(),sa[i].getNetPay());
+        }
+    }
+    public static void main(String[] args) {
+        Sawon []sawon=null; //Reference 변수
+        int inwon;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("직원 인원수는? : ");
+        inwon = Integer.parseInt(sc.nextLine());
+        sawon = new Sawon[inwon]; //인원 수 만큼 메모리 할당, 초기값은 null
+
+        inputSawonArray(sawon);
+        outputSawonArray(sawon);
+    }
+}
---
+++ javawork/Naver0314JavaProject/src/day0322/Ex08Constructor.java
@@ -0,0 +1,40 @@
+package day0322;
+/*
+        생성자(Constructor) 의 특징
+        1. 클래스 명과 동일한 메서드 명으로 만들어야 한다. (반드시)
+        2. return type 이 없다.
+        3. Overloading 이 가능하다. (여러개 생성 가능)
+        4. 생성할 때 단 한번 호출된다.
+        5. 멤버변수의 초기화를 담단한다.
+ */
+
+class Kiwi{
+    String name, addr;
+    Kiwi(){
+        System.out.println("디폴트 생성자");
+        name = "정진욱";
+        addr = "분당";
+    }
+    public void show(){
+        System.out.printf("%s 님은 %s에 살고 있어요.\n",name, addr);
+    }
+    Kiwi(String name){
+        this.name=name;
+        //addr="서울";
+    }
+    Kiwi(String name, String addr){
+        this.name=name;
+        this.addr=addr;
+    }
+}
+public class Ex08Constructor {
+    public static void main(String[] args) {
+        Kiwi k1 = new Kiwi();
+        k1.show();
+        Kiwi k2 = new Kiwi("강호동");
+        k2.show();
+        Kiwi k3 = new Kiwi("영숙이", "부산");
+        k3.show();
+
+    }
+}
